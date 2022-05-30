4943e11b51db6230ada91827aab8c4978151d7bd
Matheus Oliveira <matheus.oliveira@sciensa.com>
2022-05-30 16:55:25 -0300

    Aulas 9 e 10 - Refatorando para utilizar DTOs, MapStruct e Hibernate Validator

---
+++ cadastro/src/main/java/com/github/viniciusfcf/ifood/cadastro/dto/AtualizarRestauranteDTO.java
@@ -0,0 +1,8 @@
+package com.github.viniciusfcf.ifood.cadastro.dto;
+
+public class AtualizarRestauranteDTO {
+
+    public String nomeFantasia;
+
+}
+
--- cadastro/src/main/java/com/github/viniciusfcf/ifood/cadastro/dto/RestauranteMapper.java
+++ cadastro/src/main/java/com/github/viniciusfcf/ifood/cadastro/dto/RestauranteMapper.java
@@ -4,11 +4,24 @@ import com.github.viniciusfcf.ifood.cadastro.Restaurante;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "cdi")
 public interface RestauranteMapper {
 	
 	@Mapping(target = "nome", source = "nomeFantasia")
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "dataCriacao", ignore = true)
+	@Mapping(target = "dataAtualizacao", ignore = true)
+	@Mapping(target = "localizacao.id", ignore = true) //localizacao.id, getter???
 	public Restaurante toRestaurante(AdicionarRestauranteDTO dto);
+	
+	@Mapping(target = "nome", source = "nomeFantasia")
+	public void toRestaurante(AtualizarRestauranteDTO dto, @MappingTarget Restaurante restaurante);
+	
+	
+	@Mapping(target = "nomeFantasia", source = "nome")
+	@Mapping(target = "dataCriacao", dateFormat = "dd/MM/yyyy HH:mm:ss") // Exemplo de formatação
+	public RestauranteDTO toRestauranteDTO(Restaurante restaurante);
 	
 }
